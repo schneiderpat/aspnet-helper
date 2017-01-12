5cb343e356ad6d81878828e58142c2c828b17847
Patrick Schneider <schneiderpat@outlook.de>
2017-01-12 21:27:30 +0100

    Registered html parser

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import ItemProvider from './provider/itemProvider';
+import HtmlParser from './provider/html/htmlParser';
 
 export function activate(context: vscode.ExtensionContext) {
+
+    let itemProvider = new ItemProvider(new HtmlParser());
+    vscode.languages.registerCompletionItemProvider('html', itemProvider);
 
 }
 
