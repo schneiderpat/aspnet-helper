e34878e4373e1183f4175aca3c6537c8925c3d7f
Patrick Schneider <schneiderpat@outlook.de>
2017-01-17 10:26:05 +0100

    Added basic razor model suggestions

--- src/provider/razor/modelDeclarationInfo.ts
+++ src/provider/razor/modelDeclarationInfo.ts
@@ -5,21 +5,33 @@ import * as glob from 'glob';
 
 export default class ModelDeclarationInfo {
 
-    public getCurrentModel(document: vscode.TextDocument): string {
-        let firstLine = document.lineAt(0).text;
+    private _document: vscode.TextDocument;
+    
+    constructor(document: vscode.TextDocument) {
+        this._document = document;
+    }
+
+    public userWantsSuggestions(input: string): boolean {
+        let userRegExp = /.*@Model.$/;
+        if (userRegExp.test(input)) return true;
+        return false
+    }
+
+    public getCurrentModel(): string {
+        let firstLine = this._document.lineAt(0).text;
         let model = this.getSpecificPart(firstLine, this.modelRegExp);
         return model;
     }
 
-    public getNamespaces(document: vscode.TextDocument): string[] {
+    public getNamespaces(): string[] {
 
-        let files = this.getViewImportsFiles(document);
+        let files = this.getViewImportsFiles();
         let namespaces = this.getNamespacesFromFiles(files);
         return namespaces
     }
 
-    private getViewImportsFiles(document: vscode.TextDocument): string[] {
-        let currentDir = document.uri.fsPath;
+    private getViewImportsFiles(): string[] {
+        let currentDir = this._document.uri.fsPath;
         let files: string[] = [];
 
         while (currentDir !== vscode.workspace.rootPath) {
@@ -46,26 +58,24 @@ export default class ModelDeclarationInfo {
         return namespaces;
     }
 
-    public getProperties(model: string, namespaces: string[]): string[] {
-        let matchingFiles: string[] = [];
-        let files = this.getModelFiles();
-
-        namespaces.forEach(n => {
-            files.forEach(f => {
-                let matchingFile = this.getMatchingFiles(model, n, f);
-                if (matchingFile) matchingFiles.push(matchingFile); 
-            });
-        });
-
+    public getProperties(model: string, namespaces: string[]): vscode.CompletionItem[] {
+        let matchingFiles: string[] = this.getMatchingFiles(model, namespaces);
+        
         if (matchingFiles.length = 1) {
             let text = fs.readFileSync(matchingFiles[0], 'utf8');
-            let bodyRegExp = /public\s[a-zA-Z]+\s([a-zA-Z]+)/g;
+            let bodyRegExp = /public\s([a-zA-Z]+<?[a-zA-Z]+>?)\s([a-zA-Z]+)/g;
             let body = text.match(bodyRegExp);
             if (body) {
-                let properties: string[] = [];
+                let properties = new Array<vscode.CompletionItem>();
                 body.forEach(b => { 
-                    let prop = this.getSpecificPart(b, this.propRegExp);
-                    if (prop) properties.push(prop);
+                    let propType = this.getSpecificPart(b, this.propRegExp);
+                    let propName = this.getSpecificPart(b, this.propRegExp, 2);
+                    if (propType && propName) {
+                        let item = new vscode.CompletionItem(propName);
+                        item.kind = vscode.CompletionItemKind.Property;
+                        item.detail = propType;
+                        properties.push(item);
+                    }
                 });
                 return properties;
             }
@@ -73,14 +83,20 @@ export default class ModelDeclarationInfo {
         return [];
     }
 
-    private getModelFiles(): string[] {
+    private getMatchingFiles(model: string, namespaces: string[]): string[] {
         let modelsPattern = vscode.workspace.rootPath + path.sep + '**\\Models\\**\\*.cs';
         let viewModelsPattern = vscode.workspace.rootPath + path.sep + '**\\ViewModels\\**\\*.cs';
         let files = glob.sync(modelsPattern).concat(glob.sync(viewModelsPattern));
-        return files;
+        let matchingFiles: string[] = [];
+        namespaces.forEach(n => {
+            files.forEach(f => {
+                if (this.isMatchingFile(model, n, f)) matchingFiles.push(f); 
+            });
+        });
+        return matchingFiles;
     }
 
-    private getMatchingFiles(model: string, namespace: string, file: string): string {
+    private isMatchingFile(model: string, namespace: string, file: string): string {
         let text = fs.readFileSync(file, 'utf8');
         let namespaceRegExp = new RegExp('namespace\\s' + namespace);
         let classNameRegExp = new RegExp('class\\s' + model);
@@ -91,7 +107,7 @@ export default class ModelDeclarationInfo {
     }
 
     private modelRegExp = /.?model\s(.*)$/
-    private propRegExp = /public\s[a-zA-Z]+\s([a-zA-Z]+)/
+    private propRegExp = /public\s([a-zA-Z]+<?[a-zA-Z]+>?)\s([a-zA-Z]+)/
     private getSpecificPart(text: string, regExp: RegExp, part: number = 1): string {
         if (!regExp.test(text)) return ''
         let results = regExp.exec(text);
--- src/provider/razor/modelParser.ts
+++ src/provider/razor/modelParser.ts
@@ -10,32 +10,31 @@ export default class ModelParser implements IParser {
 
     public getParsingResults(input: string, document: vscode.TextDocument): vscode.CompletionList {
 
-        this._declarationInfo = new DeclarationInfo();
+        this._declarationInfo = new DeclarationInfo(document);
 
-        return this.getItems(input, document);
+        return this.getItems(input);
 
     }
 
-    getItems(input: string, document: vscode.TextDocument): vscode.CompletionList {
+    getItems(input: string): vscode.CompletionList {
 
-        let model = this._declarationInfo.getCurrentModel(document);
+        let userWantsSuggestions = this._declarationInfo.userWantsSuggestions(input);
+
+        if (!userWantsSuggestions) return new vscode.CompletionList();
+
+        let model = this._declarationInfo.getCurrentModel();
 
         if (!model) return new vscode.CompletionList();
 
-        let namespaces = this._declarationInfo.getNamespaces(document);
+        let namespaces = this._declarationInfo.getNamespaces();
 
         let properties = this._declarationInfo.getProperties(model, namespaces);
 
         if (properties.length != 0) {
             let suggestions = new vscode.CompletionList();
-            properties.forEach(p => {
-                let item = new vscode.CompletionItem(p);
-                suggestions.items.push(item);
-            });
+            suggestions.items = suggestions.items.concat(properties);
             return suggestions;
         }
-
-        // if (body) get properties
 
         return new vscode.CompletionList();
 
